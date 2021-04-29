163042409d16d83e0fa1fbe654cb5261a3ae3d42
unknown <ibmjavanh16@iiht.tech>
2021-04-29 18:02:42 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class First extends React.Component{
+render(){
+return
+<h1> This is my statement...</h1>
+}
+}
+ReactDOM.render(<First />, document.getElementById('root'));
